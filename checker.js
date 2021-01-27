048b633abc2289927fe9bd6e4aaa64fccdc8e823
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-27 14:59:08 -0300

    adding timezone

--- checker.js
+++ checker.js
@@ -1,13 +1,14 @@
 module.exports = function (token, tokenURI, date) {
 
+    // Prepare Modules
+    const tokenGenerator = require('./generator');
+    const moment = require('moment-timezone');
+
     // Result
     const result = {
-        old: { value: token, uri: tokenURI, date: date },
-        new: { value: null, uri: null, date: null }
+        old: { value: token, uri: tokenURI, date: moment.tz(date, 'Universal') },
+        new: { value: null, uri: null, date: moment.tz('Universal') }
     };
-
-    // Prepare Module
-    const tokenGenerator = require('./generator');
 
     // Exist OLD
     if (typeof result.old.value === "string") {
@@ -17,7 +18,7 @@ module.exports = function (token, tokenURI, date) {
         if (typeof result.old.uri !== "string") { result.new.uri = encodeURIComponent(result.old.value); } else {
             result.new.uri = result.old.uri;
         }
-    
+
     }
 
     // Nope
@@ -29,6 +30,10 @@ module.exports = function (token, tokenURI, date) {
         result.new.uri = newToken.uri;
 
     }
+
+    // Convert Clocks to String
+    result.old.date = result.old.date.format();
+    result.new.date = result.new.date.format();
 
     // Result
     return result;
