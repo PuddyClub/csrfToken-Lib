ca797672231c50b1d8477fe21fd47f0f1b743d11
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-27 23:25:28 -0300

    cookie session template

---
+++ template/cookie-session.js
@@ -0,0 +1,43 @@
+// https://stackoverflow.com/questions/34212039/redirect-to-firebase-hosting-custom-domain
+module.exports = function (varname = 'csrfToken', timeoutUpdate = 60) {
+    return (req, res, next) => {
+
+        // Prepare New Session
+        const newSession = require('../secureChecker')(req, {
+            value: req.session[varname],
+            uri: req.session[varname + '_uri'],
+            date: req.session[varname + '_date'],
+            timeoutUpdate: timeoutUpdate,
+        });
+
+        // Exist Session
+        if (newSession) {
+
+            // Set Session
+            req.session[varname] = newSession.new.value;
+            req.session[varname + '_uri'] = newSession.new.uri;
+            req.session[varname + '_date'] = newSession.new.date;
+
+            // Set csrfToken Object
+            req.csrfToken = newSession;
+
+        }
+
+        // Nope
+        else {
+
+            // Set Session
+            req.session[varname] = null;
+            req.session[varname + '_uri'] = null;
+            req.session[varname + '_date'] = null;
+
+            // Set csrfToken Object
+            req.csrfToken = { old: {}, new: {} };
+
+        }
+
+        // Complete
+        return next();
+
+    };
+};
